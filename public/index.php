3dc94eaba58962835dcfdaa45057441dad274360
Marine-Briet <marinebriet@hotmail.com>
2026-07-18 12:59:56 +0200

    init Composer, install router, working MVC entrypoint (#7 #8 #9)

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Buki\Router\Router;
+
+$router = new \Buki\Router\Router([
+    'base_folder' => str_replace('\\', '/', __DIR__),
+]);
+
+$router->get('/', function(Request $request, Response $response) {
+    $response->setContent('Bienvenue sur le site Touche pas au klaxon!');
+    return $response;
+});
+
+$router->run();
